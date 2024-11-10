ed3f2e4ce83ec0eca0095b3e2b5dd8118071340a
MohamedSaidCS <mohamed.said.10398@gmail.com>
2024-11-10 15:25:36 +0200

    fix query prepared statement

--- models/article.go
+++ models/article.go
@@ -3,13 +3,14 @@ package models
 import "github.com/MohamedSaidCS/web-scraper-api/db"
 
 type Article struct {
-	ID    int64
-	Title string `binding:"required"`
-	Link  string `binding:"required"`
+	ID       int64  `json:"id"`
+	Title    string `json:"title" binding:"required"`
+	Link     string `json:"link" binding:"required"`
+	Timesamp string `json:"timestamp"`
 }
 
 func (a *Article) Create() error {
-	query := "INSERT INTO articles (title, link) VALUES (?, ?) RETURNING id"
+	query := "INSERT INTO articles (title, link) VALUES ($1, $2) RETURNING id, timestamp"
 	stmt, err := db.DB.Prepare(query)
 	if err != nil {
 		return err
@@ -17,7 +18,7 @@ func (a *Article) Create() error {
 
 	defer stmt.Close()
 
-	err = stmt.QueryRow(a.Title, a.Link).Scan(&a.ID)
+	err = stmt.QueryRow(a.Title, a.Link).Scan(&a.ID, &a.Timesamp)
 	if err != nil {
 		return err
 	}
@@ -38,7 +39,7 @@ func GetArticles() ([]Article, error) {
 
 	for rows.Next() {
 		var article Article
-		err := rows.Scan(&article.ID, &article.Title, &article.Link)
+		err := rows.Scan(&article.ID, &article.Title, &article.Link, &article.Timesamp)
 		if err != nil {
 			return nil, err
 		}
@@ -50,14 +51,48 @@ func GetArticles() ([]Article, error) {
 }
 
 func GetArticleByID(id int64) (Article, error) {
-	query := "SELECT * FROM articles WHERE id=?"
+	query := "SELECT * FROM articles WHERE id=$1"
 	row := db.DB.QueryRow(query, id)
 
 	var article Article
-	err := row.Scan(&article.ID, &article.Title, &article.Link)
+	err := row.Scan(&article.ID, &article.Title, &article.Link, &article.Timesamp)
 	if err != nil {
 		return Article{}, err
 	}
 
 	return article, nil
+}
+
+func (a *Article) Update() error {
+	query := "UPDATE articles SET title=$1, link=$2 WHERE id=$3"
+	stmt, err := db.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()
+
+	_, err = stmt.Exec(a.Title, a.Link, a.ID)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (a *Article) Delete() error {
+	query := "DELETE FROM articles WHERE id=$1"
+	stmt, err := db.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()
+
+	_, err = stmt.Exec(a.ID)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
