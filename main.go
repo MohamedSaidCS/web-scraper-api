b4182a734cee8c10d22ed49383d579e06b179515
MohamedSaidCS <mohamed.said.10398@gmail.com>
2024-11-11 15:57:23 +0200

    concurrency and cron job for scraping

--- main.go
+++ main.go
@@ -5,14 +5,22 @@ import (
 	"github.com/MohamedSaidCS/web-scraper-api/routes"
 	"github.com/MohamedSaidCS/web-scraper-api/scraper"
 	"github.com/gin-gonic/gin"
+	"github.com/robfig/cron/v3"
 )
 
 func main() {
 	db.InitDB()
 
-	for _, site := range scraper.Sites {
-		scraper.ScrapeSite(site.URL, site.ArticleSelector, site.Extractor, 10)
-	}
+	c := cron.New()
+
+	c.AddFunc("@every 5m", func() {
+
+		for _, site := range scraper.Sites {
+			go scraper.ScrapeSite(site.URL, site.ArticleSelector, site.Extractor, 10)
+		}
+	})
+
+	c.Start()
 
 	server := gin.Default()
 
